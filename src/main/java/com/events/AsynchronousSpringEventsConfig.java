b3c105dd4a3653e8d2c2d38cccfbc21adb95ea02
ankur <www.ankurupadhyay999@gmail.com>
2017-07-17 01:08:15 +0530

    All files belong to spring event & integration exercise

---
+++ src/main/java/com/events/AsynchronousSpringEventsConfig.java
@@ -0,0 +1,22 @@
+package com.events;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.ApplicationEventMulticaster;
+import org.springframework.context.event.SimpleApplicationEventMulticaster;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+
+/**
+ * Created by nidhi on 27/3/17.
+ */
+@Configuration
+public class AsynchronousSpringEventsConfig {
+  @Bean(name = "applicationEventMulticaster")
+  public ApplicationEventMulticaster simpleApplicationEventMulticaster() {
+    SimpleApplicationEventMulticaster eventMulticaster
+        = new SimpleApplicationEventMulticaster();
+    System.out.println("Employee salary is greater than 30000 ");
+    eventMulticaster.setTaskExecutor(new SimpleAsyncTaskExecutor());
+    return eventMulticaster;
+  }
+}
